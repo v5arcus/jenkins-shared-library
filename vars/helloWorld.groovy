933fdc7ae1999b3078811ebcf84c56d1997daa51
vishantops <vsharma@noon.com>
2019-06-26 15:56:51 +0530

    updated jenkinsfile with library

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,4 +1,4 @@
-#!/usr/bin/groovy
+#!/usr/bin/env groovy
 def call(name) {
     // you can call any valid step functions from your code, just like you can from Pipeline scripts
     echo "Hello world, ${name}"
