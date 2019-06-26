6b80f7da22d4b0923e9a3cf57c491384a6fa8cfd
vishantops <vsharma@noon.com>
2019-06-26 17:45:10 +0530

    updated jenkinsfile with library

--- vars/hello.groovy
+++ vars/hello.groovy
@@ -2,3 +2,12 @@ def call(String name) {
     // you can call any valid step functions from your code, just like you can from Pipeline scripts
     echo "Hello world, ${name}"
 }
+
+def call(cutiing) {
+    script {
+        serviceName = sh (
+            script: "echo ${POD_NAME} |  cut -d '-' -f 1",
+            returnStdout: true
+            ).trim()
+        }    
+}
