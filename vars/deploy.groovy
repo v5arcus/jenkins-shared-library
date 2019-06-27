31760d772181e1bd0711d5e423047d836d3459b4
vishantops <vsharma@noon.com>
2019-06-27 15:11:27 +0530

    automated commit message

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -1,7 +1,8 @@
 def call(deploy) {
-    script {   
+    script {
         echo "We are going to deploy ${POD_NAME}"
         sh "kubectl set image deployment/${POD_NAME} ${POD_NAME}=${REGISTRY_PATH}/${POD_NAME}:${BUILD_NUMBER} -n ${ENVIRONMENT_NAME} --kubeconfig=${CLUSTER_CONFIG}"
         sh "kubectl rollout status deployment/${POD_NAME} -n ${ENVIRONMENT_NAME} --kubeconfig=${CLUSTER_CONFIG}"
+        echo "${deploy}"
     }
 }
