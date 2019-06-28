0ef776020f6bb5b0cba001468d083e826965e40d
vishantops <vsharma@noon.com>
2019-06-28 15:22:49 +0530

    automated commit message

--- vars/dildo.groovy
+++ vars/dildo.groovy
@@ -1,21 +1,10 @@
 def call() {
     return([
         parameters([
-            string(defaultValue: 'staging', name: 'CLUSTER_CONFIG', description: 'choose staging for t1/s1'),
-            choice(choices: 't0\ns0\nt1\ns1', name:'ENVIRONMENT_NAME', description: 'Environment where Pod running'),
-            string(defaultValue: "registry-intl.me-east-1.aliyuncs.com/noonpay_development", description: 'Registry Path', name: 'REGISTRY_PATH'),
-            choice(choices: 'api-gateway\ncampaign-service\ncron-manager-service\ninstrumentation-service\nload-money-service\nmarketing-panel\nmerchant-gateway\nmerchant-identity-service\nnotification-service\notp-service\np2p-service\nrecharge-service\nreporting-service\nspend-money-service\nuser-identity-service\nwithdraw-service\nbill-payments-service\ncustomer-support-dashboard\nkyc-service\nmerchant-panel\nmerchant-ops-panel', description: 'Select the name of the service need to be deployed', name: 'POD_NAME'),
-            string(defaultValue: "latest", description: 'Build Number want to deploy', name: 'BUILD_NUMBER')
+        string(defaultValue: 'staging', name: 'CLUSTER_CONFIG', description: 'choose staging for t1/s1'),
+        choice(choices: 'api-gateway\ncampaign-service\ncron-manager-service\ncustomer-support-dashboard\nnoonpay-web\ninstrumentation-service\nkyc-service\nload-money-service\nmerchant-gateway\nmerchant-identity-service\nmerchant-panel\nnotification-service\notp-service\np2p-service\nrecharge-service\nreporting-service\nspend-money-service\nuser-identity-service\nwithdraw-service\nbill-payments-service\n', description: 'Select the name of the service need to be deployed', name: 'SERVICE_NAME'),
+        string(name: 'REGISTRY_PATH', defaultValue:"registry-intl.me-east-1.aliyuncs.com/noonpay_development", description: "Enter the container registry path"),
+        choice(choices: 't0\ns0\nt1\ns1', name:'ENVIRONMENT_NAME', description: 'Environment where service needs to be deployed')
         ])
     ])
 }
-
-// properties([
-//         parameters([
-//             string(defaultValue: 'staging', name: 'CLUSTER_CONFIG', description: 'choose staging for t1/s1'),
-//             choice(choices: 't0\ns0\nt1\ns1', name:'ENVIRONMENT_NAME', description: 'Environment where Pod running'),
-//             string(defaultValue: "registry-intl.me-east-1.aliyuncs.com/noonpay_development", description: 'Registry Path', name: 'REGISTRY_PATH'),
-//             choice(choices: 'api-gateway\ncampaign-service\ncron-manager-service\ninstrumentation-service\nload-money-service\nmarketing-panel\nmerchant-gateway\nmerchant-identity-service\nnotification-service\notp-service\np2p-service\nrecharge-service\nreporting-service\nspend-money-service\nuser-identity-service\nwithdraw-service\nbill-payments-service\ncustomer-support-dashboard\nkyc-service\nmerchant-panel\nmerchant-ops-panel', description: 'Select the name of the service need to be deployed', name: 'POD_NAME'),
-//             string(defaultValue: "latest", description: 'Build Number want to deploy', name: 'BUILD_NUMBER')
-//             ])
-//     ])
