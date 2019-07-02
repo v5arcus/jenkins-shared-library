3fe24970fa81f8efea2e2f49c0816b9de273288c
vishantops <vsharma@noon.com>
2019-07-02 17:44:07 +0530

    automated commit message

--- vars/noonPipeline.groovy
+++ vars/noonPipeline.groovy
@@ -1,12 +1,12 @@
 def call() {
     node {
 
-        // def p = pipelineCfg()
+         def p = pipelineCfg()
 
         stage('listing'){
             sh 'pwd'
             sh 'ls'
-            // echo "${p.ENVIRONMENT}"
+            echo "${p.ENVIRONMENT}"
         }
 
 
