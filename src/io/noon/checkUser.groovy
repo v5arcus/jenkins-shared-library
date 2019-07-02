906454feaf647d1590afb3b893775b599b4f10c4
vishantops <vsharma@noon.com>
2019-07-02 16:09:32 +0530

    automated commit message

--- src/io/noon/checkUser.groovy
+++ src/io/noon/checkUser.groovy
@@ -1,7 +1,7 @@
 #!/usr/bin/groovy
 package io.noon;
 
-def call(CLUSTER_CONFIG) {
+def clusterConfig(CLUSTER_CONFIG) {
     sh " export ${CLUSTER_CONFIG}"
     sh " cp /var/lib/jenkins/${CLUSTER_CONFIG} ${workspace}/"
 }
