3f1e6e2a69592a40edacf1b15e36a3dec79b5507
vishantops <vsharma@noon.com>
2019-07-03 10:23:28 +0530

    automated commit message

--- vars/noonPipeline.groovy
+++ vars/noonPipeline.groovy
@@ -13,6 +13,13 @@ def call() {
             sh " cp /var/lib/jenkins/${p.CLUSTER_CONFIG} ${workspace}/"
         }
 
+        stage('Updating serviceName') {
+            serviceName = sh (
+                    script: "echo ${SERVICE_NAME} |  cut -d '-' -f 1",
+                    returnStdout: true
+                ).trim()
+        }
+
 
     }
 }
