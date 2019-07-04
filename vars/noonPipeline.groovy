9110296ee8d75ae8fc731d10abff4243dcf1de62
vishantops <vsharma@noon.com>
2019-07-04 11:06:53 +0530

    automated commit message

--- vars/noonPipeline.groovy
+++ vars/noonPipeline.groovy
@@ -17,13 +17,6 @@ def call() {
                 ).trim()
         }
 
-        // stage('Updating serviceName') {
-        //     serviceName = sh (
-        //             script: "echo ${p.SERVICE_NAME} |  cut -d '-' -f 1",
-        //             returnStdout: true
-        //         ).trim()
-        // }
-
         // stage('current image service tag') {
         //     previousVersion = sh (
         //             script: "kubectl describe deployment p.SERVICE_NAME -n p.ENVIRONMENT_NAME --kubeconfig=p.CLUSTER_CONFIG| grep Image | awk -F \"/noonpay_development/p.SERVICE_NAME:\" \'{print $2}\' ",
@@ -47,6 +40,37 @@ def call() {
             echo "We are going to deploy ${p.SERVICE_NAME}"
             sh "kubectl set image deployment/${p.SERVICE_NAME} ${p.SERVICE_NAME}=${p.REGISTRY_PATH}/${p.SERVICE_NAME}:${BUILD_NUMBER} -n ${p.ENVIRONMENT_NAME} --kubeconfig=${p.CLUSTER_CONFIG}"
             sh "kubectl rollout status deployment/${p.SERVICE_NAME} -n ${p.ENVIRONMENT_NAME} --kubeconfig=${p.CLUSTER_CONFIG}"
+
+            if ("${p.SERVICE_NAME}" == 'merchant-gateway') {
+                sh "ssh kubecli@47.91.111.100 -p 2290 yaml_cli -f /home/kubecli/k8s-manifest/${p.ENVIRONMENT_NAME}/helm-${p.ENVIRONMENT_NAME}/application/values.yaml -n gateway:image_tag ${BUILD_NUMBER}"
+            }
+            else if ("${p.SERVICE_NAME}" == 'merchant-identity-service') {
+                sh "ssh kubecli@47.91.111.100 -p 2290 yaml_cli -f /home/kubecli/k8s-manifest/${p.ENVIRONMENT_NAME}/helm-${p.ENVIRONMENT_NAME}/application/values.yaml -n identity:image_tag ${BUILD_NUMBER}"
+            }
+            else if ("${p.SERVICE_NAME}" == 'merchant-panel') {
+                sh "ssh kubecli@47.91.111.100 -p 2290 yaml_cli -f /home/kubecli/k8s-manifest/${p.ENVIRONMENT_NAME}/helm-${p.ENVIRONMENT_NAME}/application/values.yaml -n panel:image_tag ${BUILD_NUMBER}"
+            }
+            else if ("${p.SERVICE_NAME}" == 'merchant-ops-panel') {
+                sh "ssh kubecli@47.91.111.100 -p 2290 yaml_cli -f /home/kubecli/k8s-manifest/${p.ENVIRONMENT_NAME}/helm-${p.ENVIRONMENT_NAME}/application/values.yaml -n merchant:image_tag ${BUILD_NUMBER}"
+            }
+            else {
+                sh "ssh kubecli@47.91.111.100 -p 2290 yaml_cli -f /home/kubecli/k8s-manifest/${p.ENVIRONMENT_NAME}/helm-${p.ENVIRONMENT_NAME}/application/values.yaml -n ${serviceName}:image_tag ${BUILD_NUMBER}"
+            }
+        }
+
+        stage ('Is our Pod running ?') {
+            sleep (5)
+            status = sh (
+                script: "kubectl get pods -n ${p.ENVIRONMENT_NAME} -o=jsonpath='{range .items[*]}{@.status.containerStatuses[0].ready}:{@.metadata.name}' --kubeconfig=${p.CLUSTER_CONFIG}| grep ${p.SERVICE_NAME} |  cut -d ':' -f 1", 
+                returnStdout: true
+            ).trim()
+            if (status == "true") {
+                mail_status = "Success"
+            }
+            else {
+                mail_status = "Failure"
+            }
+                    
         }
 
     }
